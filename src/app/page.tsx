1addc91b56b9ce9b821b13db1c07a810a84b31ea
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-10 19:56:53 +0000

    feat: create tinder bio form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import { useChat } from 'ai/react';
 import { ChangeEvent, FormEvent, useState } from 'react';
+import { FaGithub, FaInternetExplorer } from 'react-icons/fa';
+import { SiTinder } from 'react-icons/si';
+import { TbBrandTinder } from 'react-icons/tb';
 
-type WritingStyleProps = 'Serious' | 'Funny' | 'Clever' | 'None';
+type WritingStyleProps = 'Serious' | 'Funny' | 'Clever';
 
 export default function Home() {
   const [tinderBio, setTinderBio] = useState('');
-  const [writingStyle, setWritingStyle] = useState<WritingStyleProps>('Funny');
+  const [writingStyle, setWritingStyle] = useState<WritingStyleProps>();
 
   const { input, handleInputChange, handleSubmit, isLoading, messages } =
     useChat({
@@ -31,63 +34,119 @@ export default function Home() {
     aiMessage?.role === 'assistant' ? aiMessage.content : null;
 
   return (
-    <main>
-      <form className='mx-auto max-w-sm space-y-4' onSubmit={onSubmit}>
-        <textarea
-          defaultValue={input}
-          onChange={onInputChange}
-          rows={4}
-          required
-          placeholder={
-            'Enter your exiting tinder bio or enter some keywords about yourself.'
-          }
-        />
+    <div className='flex flex-col min-h-screen gap-8'>
+      <header>
+        <div className='navbar bg-base-100'>
+          <a className='btn btn-ghost normal-case text-xl'>
+            <TbBrandTinder size={50} /> tinderbio.io
+          </a>
+        </div>
+        <div className='flex flex-col w-full'>
+          <div className='divider'></div>
+        </div>
+      </header>
+      <main className='prose text-center flex flex-col justify-center items-center container flex-grow gap-8'>
+        <h1>Transform Your Tinder Game</h1>
+        <h2>Let AI take the guesswork out of creating the perfect bio.</h2>
+        <form className='flex flex-col gap-8' onSubmit={onSubmit}>
+          <div className='form-control'>
+            <label className='label'>
+              <span className='label-text'>
+                <strong>Step 1.</strong> Copy your current bio (or just write
+                some keywords)
+              </span>
+            </label>
+            <textarea
+              className='textarea textarea-bordered textarea-lg w-full max-w-xs'
+              placeholder='e.g. Young professional looking for a partner in crime'
+              defaultValue={input}
+              onChange={onInputChange}
+              required
+            ></textarea>
+          </div>
 
-        <label htmlFor='writingStyle'>Choose a writing style:</label>
-        <select
-          id='writingStyle'
-          defaultValue={writingStyle}
-          onChange={(e) => setWritingStyle(e.target.value as WritingStyleProps)}
-        >
-          <option value='Serious'>Serious</option>
-          <option value='Funny'>Funny</option>
-          <option value='Clever'>Clever</option>
-          <option value='None'>None</option>
-        </select>
+          <div className='form-control'>
+            <label className='label'>
+              <span className='label-text'>
+                <strong>Step 2.</strong> Pick your writing style
+              </span>
+            </label>
+            <select
+              className='select select-bordered select-lg w-full max-w-xs'
+              defaultValue={writingStyle || ''}
+              onChange={(e) =>
+                setWritingStyle(e.target.value as WritingStyleProps)
+              }
+              required
+            >
+              <option disabled value=''>
+                Pick one
+              </option>
+              <option value='Serious'>Serious</option>
+              <option value='Funny'>Funny</option>
+              <option value='Clever'>Clever</option>
+            </select>
+          </div>
 
-        <button type='submit' disabled={isLoading}>
-          {isLoading ? 'Loading...' : 'Submit'}
-        </button>
-      </form>
+          <button className='btn' type='submit' disabled={isLoading}>
+            {isLoading ? 'Loading...' : 'Create your bio 👉'}
+          </button>
+        </form>
 
-      <hr className='h-px bg-gray-700 border-1 dark:bg-gray-700' />
-      <output className='space-y-10 my-10'>
-        {generatedBios && (
-          <>
-            <div>
-              <h2 className='sm:text-4xl text-3xl font-bold text-slate-900 mx-auto'>
-                Your generated bios
-              </h2>
-            </div>
-            <div className='space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto'>
+        <output>
+          {generatedBios && (
+            <>
               {generatedBios
                 .substring(generatedBios.indexOf('1') + 3)
                 .split('2.')
                 .map((generatedBio, index) => (
-                  <div
-                    className='bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border'
-                    onClick={() => {
-                      navigator.clipboard.writeText(generatedBio);
-                    }}
-                    key={index}
-                  >
-                    <p>{generatedBio}</p>
+                  <div key={index} className='chat chat-start'>
+                    <div className='chat-header'>
+                      Cupid{' '}
+                      <time className='text-xs opacity-50'>1 second ago</time>
+                    </div>
+                    <div
+                      className='chat-bubble'
+                      onClick={() => {
+                        navigator.clipboard.writeText(generatedBio);
+                      }}
+                    >
+                      {generatedBio}
+                    </div>
+                    <div className='chat-footer opacity-50'>Seen</div>
                   </div>
                 ))}
-            </div>
-          </>
-        )}
-      </output>
-    </main>
+            </>
+          )}
+        </output>
+      </main>
+      <footer className='footer footer-center p-10 bg-primary text-primary-content'>
+        <aside>
+          <TbBrandTinder size={50} />
+          <p>
+            Powered by{' '}
+            <a
+              className='font-bold link'
+              href='https://openai.com/blog/chatgpt'
+              target='_blank'
+            >
+              ChatGPT
+            </a>
+            .
+          </p>
+          <p>Copyright © 2023 - All right reserved</p>
+        </aside>
+        <nav>
+          <div className='grid grid-flow-col gap-4'>
+            <a href='https://github.com/Michael-Hutchinson' target='_blank'>
+              <FaGithub size={25} />
+            </a>
+            <a href='https://michaelhutchinson.me' target='_blank'>
+              <FaInternetExplorer size={25} />
+            </a>
+          </div>
+        </nav>
+      </footer>
+    </div>
   );
 }
