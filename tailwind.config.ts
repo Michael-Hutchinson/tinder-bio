5f4a738faba69a43eb4ce53fcb296714316a757e
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-10 19:56:34 +0000

    feat: set theme and plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    container: {
+      center: true,
     },
   },
-  plugins: [require('daisyui'), require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ['valentine'],
+  },
 };
 export default config;
