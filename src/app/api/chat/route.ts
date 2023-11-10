f93f3e864ae21295162bf9f8fd1b9acb91bd2383
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-10 16:56:32 +0000

    refactor: add in error handling for api requests

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,10 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import OpenAI from 'openai';
 
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('OPENAI_API_KEY environment variable is not set');
+}
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -10,18 +14,39 @@ export const runtime = 'edge';
 export async function POST(req: Request) {
   const { writingStyle, tinderBio } = await req.json();
 
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: [
-      {
-        role: 'user',
-        content: `Generate 2 ${writingStyle} tinder biographies. Make sure each response is less than 45 characters. You can use their existing bio as a starting point: ${tinderBio}. Make sure each response is clearly labeled "1." and "2."`,
-      },
-    ],
-  });
+  if (!writingStyle || !tinderBio) {
+    return {
+      status: 400,
+      body: 'writingStyle and tinderBio are required',
+    };
+  }
 
-  const stream = OpenAIStream(response);
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages: [
+        {
+          role: 'user',
+          content: `Generate 2 ${writingStyle} tinder biographies. Make sure each response is less than 500 characters. ${
+            tinderBio
+              ? `You can use their existing bio as a starting point: ${tinderBio}${
+                  tinderBio.slice(-1) === '.' ? '' : '.'
+                }`
+              : ''
+          }. Make sure each response is clearly labeled "1." and "2." and make sure you dont include any special characters or numbers in the response.`,
+        },
+      ],
+    });
 
-  return new StreamingTextResponse(stream);
+    const stream = OpenAIStream(response);
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error(error);
+    return {
+      status: 500,
+      body: 'An error occurred while generating the tinder biographies',
+    };
+  }
 }
